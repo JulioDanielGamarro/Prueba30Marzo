75fe467a2840de3fa132ef84b954c8d7dabc1232
JulioDanielGamarro <62576144+JulioDanielGamarro@users.noreply.github.com>
2020-04-02 13:43:11 -0600

    Add files via upload

---
+++ Prueba30Marzo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba30marzo;
+
+/**
+ *
+ * @author Daniel Gamarro
+ */
+public class Prueba30Marzo {
+    
+public static void main(String[]args){
+    System.out.println("Julio Daniel Gamarro Cano");
+    System.out.println("1ra. Ave. 1-22 zona 1 Huehuetenango");
+    System.out.println("59032907");
+    
+}
+    
+}
